744925bab059dd80dd3f8a7008538b6344b9b734
BraRodri <38147967+BraRodri@users.noreply.github.com>
2020-07-15 17:34:52 -0500

    Update CORS

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const routesUser = require ('./routes/user');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+//Load routings
+const routesUser = require("./routes/user");
 
-app.use('/api', routesUser);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Configure Header HTTP - Configuración de los CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
+//Router Basic
+app.use("/api", routesUser);
 
 module.exports = app;
