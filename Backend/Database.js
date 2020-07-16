6bed935f24527eb59f32bdca517efe7aee2cbe85
BraRodri <38147967+BraRodri@users.noreply.github.com>
2020-07-16 13:55:38 -0500

    New line, useCreateIndex

--- Backend/Database.js
+++ Backend/Database.js
@@ -7,7 +7,7 @@ async function ConnectionWithMongoDB({ db_port, db_host, db_name }) {
   const uri = `mongodb://new-user:QYERh9SuE3oQ4pYP@instagramclone-shard-00-00.uqktf.mongodb.net:27017,instagramclone-shard-00-01.uqktf.mongodb.net:27017,instagramclone-shard-00-02.uqktf.mongodb.net:27017/InstagramClone?ssl=true&replicaSet=atlas-p6yxya-shard-0&authSource=admin&retryWrites=true&w=majority`;
   mongoose.connect(
     uri,
-    { useNewUrlParser: true, useUnifiedTopology: true },
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
     (err, info) => {
       if (err) {
         console.log("Error in the Connection to Mongo");
