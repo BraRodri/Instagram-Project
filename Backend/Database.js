8a8b9dc80dd67ffc863b426590fd1ed8338ce9e3
BraRodri <38147967+BraRodri@users.noreply.github.com>
2020-07-16 13:47:48 -0500

    Update connect mongo atlas
    Version nodejs in MongoAtlas is 2.2.12

--- Backend/Database.js
+++ Backend/Database.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 mongoose.set("useFindAndModify", false);
 
 async function ConnectionWithMongoDB({ db_port, db_host, db_name }) {
-  const uri = `mongodb+srv://new-user:QYERh9SuE3oQ4pYP@instagramclone.uqktf.mongodb.net/InstagramClone?retryWrites=true&w=majority`;
+  //const uri = `mongodb+srv://new-user:QYERh9SuE3oQ4pYP@instagramclone.uqktf.mongodb.net/InstagramClone?retryWrites=true&w=majority`;
+  const uri = `mongodb://new-user:QYERh9SuE3oQ4pYP@instagramclone-shard-00-00.uqktf.mongodb.net:27017,instagramclone-shard-00-01.uqktf.mongodb.net:27017,instagramclone-shard-00-02.uqktf.mongodb.net:27017/InstagramClone?ssl=true&replicaSet=atlas-p6yxya-shard-0&authSource=admin&retryWrites=true&w=majority`;
   mongoose.connect(
     uri,
     { useNewUrlParser: true, useUnifiedTopology: true },
